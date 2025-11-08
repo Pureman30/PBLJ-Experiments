06c2fc16cf75df5c76512264ae914c7a3c52dc24
Pureman30 <aayushsharmaa29@gmail.com>
2025-11-08 15:15:49 +0530

    Create App.java

---
+++ Experiment-7,8,9/Exp-8/Part-a/Spring-DI-JavaConfig/src/App.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Student s = context.getBean(Student.class);
+        s.showDetails();
+    }
+}
