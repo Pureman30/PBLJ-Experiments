1032fa8609849bed19f4fcaa6d840108cea54b43
Pureman30 <aayushsharmaa29@gmail.com>
2025-11-08 15:16:41 +0530

    Add Course class with courseName property

---
+++ Experiment-7,8,9/Exp-8/Part-a/Spring-DI-JavaConfig/src/Course.java
@@ -0,0 +1,12 @@
+public class Course {
+
+    private String courseName;
+
+    public Course() {
+        this.courseName = "Spring Framework";
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
